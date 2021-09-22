76788e9871dc4ac891e758681d0a1d2c021c308f
Joydeep Bandyopadhyay <joydeep@shyamfuture>
2021-09-22 16:52:20 +0530

    chart 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,13 +63,17 @@
           </div>
           <!-- ./col -->
     </div>
+
     <div class="approval-time-wrap row">
       <div class="col-md-12">
-        <div id="chart">
-      </div>
+        <div id="chart"></div>
       </div>  
-      
+   </div> 
+   <div class="approval-time-wrap row">
+    <div class="col-md-12">
+      <div id="chart2"></div>
     </div>  
+ </div>  
 
 @stop
 
@@ -79,10 +83,10 @@
 
 @section('js')
 <script type="text/javascript">
-loadChart();
+loadChart1();
 
 
-function loadChart() {
+function loadChart1() {
 
 $.ajax({
   url:"{{ route('approval.analytics') }}",
@@ -136,9 +140,33 @@ var chart = new ApexCharts(document.querySelector("#chart"), options);
 
 chart.render();
 
-  }
+}
 });
+}
+loadChart2();
+function loadChart2() {
+  var options = {
+          series: [44, 55, 13, 43, 22],
+          chart: {
+          width: 380,
+          type: 'pie',
+        },
+        labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }]
+        };
 
+        var chart = new ApexCharts(document.querySelector("#chart2"), options);
+        chart.render();
 }
 
 

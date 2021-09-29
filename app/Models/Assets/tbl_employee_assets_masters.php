110ca4c2563e20f5cc2721e84a69d743d4cbcec0
Joydeep Bandyopadhyay <joydeep@shyamfuture>
2021-09-29 18:06:22 +0530

    model fix

--- app/Models/Assets/tbl_employee_assets_masters.php
+++ app/Models/Assets/tbl_employee_assets_masters.php
@@ -10,6 +10,10 @@ class tbl_employee_assets_masters extends Model
 {
     use HasFactory;
 
+    protected $casts = [
+        'created_at' => 'datetime:d-m-Y H:i A'
+    ];
+    
     public function asset() {
         return $this->belongsTo(AssetsMasters::class, 'asset_id', 'id');
     }

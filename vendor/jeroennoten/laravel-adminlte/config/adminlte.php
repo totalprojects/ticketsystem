17c982e381647ad1310c69e5daf8c45142b8ed90
Joydeep <joydeep@shyamfuture.com>
2021-10-05 12:43:43 +0530

    changes

--- vendor/jeroennoten/laravel-adminlte/config/adminlte.php
+++ vendor/jeroennoten/laravel-adminlte/config/adminlte.php
@@ -12,11 +12,11 @@ return [
     | For detailed instructions you can look the title section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
     |
-     */
+    */
 
-    'title'                                   => 'It Complaince & Audit',
-    'title_prefix'                            => '',
-    'title_postfix'                           => '',
+    'title' => 'AdminLTE 3',
+    'title_prefix' => '',
+    'title_postfix' => '',
 
     /*
     |--------------------------------------------------------------------------
@@ -28,10 +28,10 @@ return [
     | For detailed instructions you can look the favicon section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
     |
-     */
+    */
 
-    'use_ico_only'                            => false,
-    'use_full_favicon'                        => false,
+    'use_ico_only' => false,
+    'use_full_favicon' => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -43,14 +43,14 @@ return [
     | For detailed instructions you can look the logo section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
     |
-     */
+    */
 
-    'logo'                                    => '<b>Admin</b>LTE',
-    'logo_img'                                => 'vendor/adminlte/dist/img/AdminLTELogo.png',
-    'logo_img_class'                          => 'brand-image img-circle elevation-3',
-    'logo_img_xl'                             => null,
-    'logo_img_xl_class'                       => 'brand-image-xs',
-    'logo_img_alt'                            => 'AdminLTE',
+    'logo' => '<b>Admin</b>LTE',
+    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo_img_class' => 'brand-image img-circle elevation-3',
+    'logo_img_xl' => null,
+    'logo_img_xl_class' => 'brand-image-xs',
+    'logo_img_alt' => 'AdminLTE',
 
     /*
     |--------------------------------------------------------------------------
@@ -62,14 +62,14 @@ return [
     | For detailed instructions you can look the user menu section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
     |
-     */
+    */
 
-    'usermenu_enabled'                        => true,
-    'usermenu_header'                         => false,
-    'usermenu_header_class'                   => 'bg-primary',
-    'usermenu_image'                          => false,
-    'usermenu_desc'                           => false,
-    'usermenu_profile_url'                    => false,
+    'usermenu_enabled' => true,
+    'usermenu_header' => false,
+    'usermenu_header_class' => 'bg-primary',
+    'usermenu_image' => false,
+    'usermenu_desc' => false,
+    'usermenu_profile_url' => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -81,13 +81,13 @@ return [
     | For detailed instructions you can look the layout section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Layout-and-Styling-Configuration
     |
-     */
+    */
 
-    'layout_topnav'                           => null,
-    'layout_boxed'                            => null,
-    'layout_fixed_sidebar'                    => null,
-    'layout_fixed_navbar'                     => null,
-    'layout_fixed_footer'                     => null,
+    'layout_topnav' => null,
+    'layout_boxed' => null,
+    'layout_fixed_sidebar' => null,
+    'layout_fixed_navbar' => null,
+    'layout_fixed_footer' => null,
 
     /*
     |--------------------------------------------------------------------------
@@ -99,14 +99,14 @@ return [
     | For detailed instructions you can look the auth classes section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Layout-and-Styling-Configuration
     |
-     */
+    */
 
-    'classes_auth_card'                       => 'card-outline card-primary',
-    'classes_auth_header'                     => '',
-    'classes_auth_body'                       => '',
-    'classes_auth_footer'                     => '',
-    'classes_auth_icon'                       => '',
-    'classes_auth_btn'                        => 'btn-flat btn-primary',
+    'classes_auth_card' => 'card-outline card-primary',
+    'classes_auth_header' => '',
+    'classes_auth_body' => '',
+    'classes_auth_footer' => '',
+    'classes_auth_icon' => '',
+    'classes_auth_btn' => 'btn-flat btn-primary',
 
     /*
     |--------------------------------------------------------------------------
@@ -118,19 +118,19 @@ return [
     | For detailed instructions you can look the admin panel classes here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Layout-and-Styling-Configuration
     |
-     */
+    */
 
-    'classes_body'                            => '',
-    'classes_brand'                           => '',
-    'classes_brand_text'                      => '',
-    'classes_content_wrapper'                 => '',
-    'classes_content_header'                  => '',
-    'classes_content'                         => '',
-    'classes_sidebar'                         => 'sidebar-dark-primary elevation-4',
-    'classes_sidebar_nav'                     => '',
-    'classes_topnav'                          => 'navbar-white navbar-light',
-    'classes_topnav_nav'                      => 'navbar-expand',
-    'classes_topnav_container'                => 'container',
+    'classes_body' => '',
+    'classes_brand' => '',
+    'classes_brand_text' => '',
+    'classes_content_wrapper' => '',
+    'classes_content_header' => '',
+    'classes_content' => '',
+    'classes_sidebar' => 'sidebar-dark-primary elevation-4',
+    'classes_sidebar_nav' => '',
+    'classes_topnav' => 'navbar-white navbar-light',
+    'classes_topnav_nav' => 'navbar-expand',
+    'classes_topnav_container' => 'container',
 
     /*
     |--------------------------------------------------------------------------
@@ -142,17 +142,17 @@ return [
     | For detailed instructions you can look the sidebar section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Layout-and-Styling-Configuration
     |
-     */
+    */
 
-    'sidebar_mini'                            => true,
-    'sidebar_collapse'                        => false,
-    'sidebar_collapse_auto_size'              => false,
-    'sidebar_collapse_remember'               => false,
+    'sidebar_mini' => true,
+    'sidebar_collapse' => false,
+    'sidebar_collapse_auto_size' => false,
+    'sidebar_collapse_remember' => false,
     'sidebar_collapse_remember_no_transition' => true,
-    'sidebar_scrollbar_theme'                 => 'os-theme-light',
-    'sidebar_scrollbar_auto_hide'             => 'l',
-    'sidebar_nav_accordion'                   => true,
-    'sidebar_nav_animation_speed'             => 300,
+    'sidebar_scrollbar_theme' => 'os-theme-light',
+    'sidebar_scrollbar_auto_hide' => 'l',
+    'sidebar_nav_accordion' => true,
+    'sidebar_nav_animation_speed' => 300,
 
     /*
     |--------------------------------------------------------------------------
@@ -164,15 +164,15 @@ return [
     | For detailed instructions you can look the right sidebar section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Layout-and-Styling-Configuration
     |
-     */
+    */
 
-    'right_sidebar'                           => false,
-    'right_sidebar_icon'                      => 'fas fa-cogs',
-    'right_sidebar_theme'                     => 'dark',
-    'right_sidebar_slide'                     => true,
-    'right_sidebar_push'                      => true,
-    'right_sidebar_scrollbar_theme'           => 'os-theme-light',
-    'right_sidebar_scrollbar_auto_hide'       => 'l',
+    'right_sidebar' => false,
+    'right_sidebar_icon' => 'fas fa-cogs',
+    'right_sidebar_theme' => 'dark',
+    'right_sidebar_slide' => true,
+    'right_sidebar_push' => true,
+    'right_sidebar_scrollbar_theme' => 'os-theme-light',
+    'right_sidebar_scrollbar_auto_hide' => 'l',
 
     /*
     |--------------------------------------------------------------------------
@@ -184,16 +184,16 @@ return [
     | For detailed instructions you can look the urls section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
     |
-     */
+    */
 
-    'use_route_url'                           => false,
-    'dashboard_url'                           => 'home',
-    'logout_url'                              => 'logout',
-    'login_url'                               => 'login',
-    'register_url'                            => 'register',
-    'password_reset_url'                      => 'password/reset',
-    'password_email_url'                      => 'password/email',
-    'profile_url'                             => false,
+    'use_route_url' => false,
+    'dashboard_url' => 'home',
+    'logout_url' => 'logout',
+    'login_url' => 'login',
+    'register_url' => 'register',
+    'password_reset_url' => 'password/reset',
+    'password_email_url' => 'password/email',
+    'profile_url' => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -205,11 +205,11 @@ return [
     | For detailed instructions you can look the laravel mix section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Other-Configuration
     |
-     */
+    */
 
-    'enabled_laravel_mix'                     => false,
-    'laravel_mix_css_path'                    => 'css/app.css',
-    'laravel_mix_js_path'                     => 'js/app.js',
+    'enabled_laravel_mix' => false,
+    'laravel_mix_css_path' => 'css/app.css',
+    'laravel_mix_js_path' => 'js/app.js',
 
     /*
     |--------------------------------------------------------------------------
@@ -221,36 +221,36 @@ return [
     | For detailed instructions you can look here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Menu-Configuration
     |
-     */
+    */
 
-    'menu'                                    => [
+    'menu' => [
         [
-            'text'   => 'search',
+            'text' => 'search',
             'search' => true,
-            'topnav' => true
+            'topnav' => true,
         ],
         [
             'text' => 'blog',
             'url'  => 'admin/blog',
-            'can'  => 'manage-blog'
+            'can'  => 'manage-blog',
         ],
         [
             'text'        => 'pages',
             'url'         => 'admin/pages',
             'icon'        => 'far fa-fw fa-file',
             'label'       => 4,
-            'label_color' => 'success'
+            'label_color' => 'success',
         ],
         ['header' => 'account_settings'],
         [
             'text' => 'profile',
             'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user'
+            'icon' => 'fas fa-fw fa-user',
         ],
         [
             'text' => 'change_password',
             'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock'
+            'icon' => 'fas fa-fw fa-lock',
         ],
         [
             'text'    => 'multilevel',
@@ -258,7 +258,7 @@ return [
             'submenu' => [
                 [
                     'text' => 'level_one',
-                    'url'  => '#'
+                    'url'  => '#',
                 ],
                 [
                     'text'    => 'level_one',
@@ -266,7 +266,7 @@ return [
                     'submenu' => [
                         [
                             'text' => 'level_two',
-                            'url'  => '#'
+                            'url'  => '#',
                         ],
                         [
                             'text'    => 'level_two',
@@ -274,38 +274,38 @@ return [
                             'submenu' => [
                                 [
                                     'text' => 'level_three',
-                                    'url'  => '#'
+                                    'url'  => '#',
                                 ],
                                 [
                                     'text' => 'level_three',
-                                    'url'  => '#'
-                                ]
-                            ]
-                        ]
-                    ]
+                                    'url'  => '#',
+                                ],
+                            ],
+                        ],
+                    ],
                 ],
                 [
                     'text' => 'level_one',
-                    'url'  => '#'
-                ]
-            ]
+                    'url'  => '#',
+                ],
+            ],
         ],
         ['header' => 'labels'],
         [
             'text'       => 'important',
             'icon_color' => 'red',
-            'url'        => '#'
+            'url'        => '#',
         ],
         [
             'text'       => 'warning',
             'icon_color' => 'yellow',
-            'url'        => '#'
+            'url'        => '#',
         ],
         [
             'text'       => 'information',
             'icon_color' => 'cyan',
-            'url'        => '#'
-        ]
+            'url'        => '#',
+        ],
     ],
 
     /*
@@ -318,16 +318,16 @@ return [
     | For detailed instructions you can look the menu filters section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Menu-Configuration
     |
-     */
+    */
 
-    'filters'                                 => [
+    'filters' => [
         JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
         JeroenNoten\LaravelAdminLte\Menu\Filters\HrefFilter::class,
         JeroenNoten\LaravelAdminLte\Menu\Filters\SearchFilter::class,
         JeroenNoten\LaravelAdminLte\Menu\Filters\ActiveFilter::class,
         JeroenNoten\LaravelAdminLte\Menu\Filters\ClassesFilter::class,
         JeroenNoten\LaravelAdminLte\Menu\Filters\LangFilter::class,
-        JeroenNoten\LaravelAdminLte\Menu\Filters\DataFilter::class
+        JeroenNoten\LaravelAdminLte\Menu\Filters\DataFilter::class,
     ],
 
     /*
@@ -340,79 +340,79 @@ return [
     | For detailed instructions you can look the plugins section here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Other-Configuration
     |
-     */
+    */
 
-    'plugins'                                 => [
-        'Datatables'  => [
+    'plugins' => [
+        'Datatables' => [
             'active' => false,
-            'files'  => [
+            'files' => [
                 [
-                    'type'     => 'js',
-                    'asset'    => false,
-                    'location' => '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js'
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js',
                 ],
                 [
-                    'type'     => 'js',
-                    'asset'    => false,
-                    'location' => '//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js'
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js',
                 ],
                 [
-                    'type'     => 'css',
-                    'asset'    => false,
-                    'location' => '//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css'
-                ]
-            ]
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css',
+                ],
+            ],
         ],
-        'Select2'     => [
+        'Select2' => [
             'active' => false,
-            'files'  => [
+            'files' => [
                 [
-                    'type'     => 'js',
-                    'asset'    => false,
-                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js'
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js',
                 ],
                 [
-                    'type'     => 'css',
-                    'asset'    => false,
-                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css'
-                ]
-            ]
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css',
+                ],
+            ],
         ],
-        'Chartjs'     => [
+        'Chartjs' => [
             'active' => false,
-            'files'  => [
+            'files' => [
                 [
-                    'type'     => 'js',
-                    'asset'    => false,
-                    'location' => '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js'
-                ]
-            ]
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js',
+                ],
+            ],
         ],
         'Sweetalert2' => [
             'active' => false,
-            'files'  => [
+            'files' => [
                 [
-                    'type'     => 'js',
-                    'asset'    => false,
-                    'location' => '//cdn.jsdelivr.net/npm/sweetalert2@8'
-                ]
-            ]
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.jsdelivr.net/npm/sweetalert2@8',
+                ],
+            ],
         ],
-        'Pace'        => [
+        'Pace' => [
             'active' => false,
-            'files'  => [
+            'files' => [
                 [
-                    'type'     => 'css',
-                    'asset'    => false,
-                    'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/blue/pace-theme-center-radar.min.css'
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/blue/pace-theme-center-radar.min.css',
                 ],
                 [
-                    'type'     => 'js',
-                    'asset'    => false,
-                    'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js'
-                ]
-            ]
-        ]
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js',
+                ],
+            ],
+        ],
     ],
 
     /*
@@ -424,7 +424,7 @@ return [
     |
     | For detailed instructions you can look the livewire here:
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Other-Configuration
-     */
+    */
 
-    'livewire'                                => false
+    'livewire' => false,
 ];
